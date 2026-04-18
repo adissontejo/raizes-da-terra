4e15e57199aa3a7286ddbe72f8684e331f7e0e4e
Wisla Alves Argolo <wisla.argolo.133@ufrn.edu.br>
2026-04-18 16:43:44 -0300

    chore: add initial backend dependencies (#10)

---
+++ rdt-api/src/test/java/br/ufrn/imd/rdt_api/RdtApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.ufrn.imd.rdt_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RdtApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
