3ecfd83fb76127b6045a62f4b0e16fc8abcbb949
Wisla Alves Argolo <wisla.argolo.133@ufrn.edu.br>
2026-05-09 13:37:12 -0300

    fix: add default prefix to image upload endpoints (#54)

--- rdt-api/src/main/java/br/ufrn/imd/rdt_api/controller/ImageUploadController.java
+++ rdt-api/src/main/java/br/ufrn/imd/rdt_api/controller/ImageUploadController.java
@@ -5,14 +5,11 @@ import br.ufrn.imd.rdt_api.service.ImageStorage.ImageStorageService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestPart;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 @RestController
-@RequestMapping("/uploads")
+@RequestMapping("/api/uploads")
 public class ImageUploadController {
 
     private final ImageStorageService imageStorageService;
